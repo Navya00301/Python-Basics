1f1600afcb7dc7cbb25aa721af0443d31ae1cafa
Navya00301 <navyamaria2005@gmail.com>
2026-02-20 14:25:24 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,88 @@
+# a= "Hello World"
+# print(a.upper())
+
+# a= " Hello , World! "
+# print(a.strip())
+
+# a="Hello World!"
+# print(a.split(" "))
+
+# a="Hello World!"
+# print(a.replace("H","J"))
+
+# age=36
+# txt=f"My name is John,I am {age}"
+# print(txt)
+
+# txt="I live in Kochi, Kochi has a special feeling"
+# x=txt.count("Kochi")
+# print(x)
+
+# x=5
+# y=3
+# print(x%y)
+
+# x=2
+# y=5
+# print(x**y)
+
+# x=15
+# y=2
+# print(x//y)
+
+# x=5
+# y=3
+# print(x==y)
+
+# print(x>3 and x<10)
+
+# print(not (x>3 and x<10))
+
+# x=["apple","banana"]
+# y=["apple","banana"]
+# z=x
+# print(x ==y)
+
+# x=["apple","banana"]
+# print("banana" in x)
+
+# a=200
+# b=33
+
+# if b>a:
+#     print("b is greater than a")
+# elif a==b:
+#     print("a and b are equal")
+# else:
+#     print("a is greater than b")
+
+# x=int(input('enter a number'))
+
+# if x>10:
+#     print("Above ten")
+#     if x>20:
+#         print("and also above 20!")
+# else:
+#     print("but not above 20")
+# if x>0:
+#     print("positive number")
+# elif x==0:
+#     print("number is zero")
+# else:
+#     print("negative number")
+
+# if x%2==0:
+#     print("even number")
+# else:
+#     print("odd number")
+
+# if x%3==0:
+#     print("number divisible by 3")
+# elif x%7==0 and x%5==0:
+#     print("both 7 and 5 are divisible")
+# else:
+#     print("not divisible by 3")
+
+x=5
+x%=2
+print(x)
---
+++ var.py
@@ -0,0 +1,39 @@
+a=1
+b=2
+c=a+b
+print(c)
+
+
+# Age=12.678888888
+# print(Age)
+# print(type(Age))
+
+# print(type(b))
+
+# name1="Navya"
+
+# print(type(name1))
+
+# num1=5+9j
+# print(type(num1))
+
+
+str1="python Fullstack "
+# print(str1)
+
+# print(str1[5])
+
+# print(str1[-10])
+
+# print(str1[0:6])
+
+# print(str1[-10:-1])
+
+# str2="Internship Python "
+# print(str2[1])
+
+# print(str2[-1])
+
+# print(str2[0:10])
+
+# print(str2[-7:-1])
