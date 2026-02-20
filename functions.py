d68547710c14612025e98c8a57b7e26b53fdc679
Navya00301 <navyamaria2005@gmail.com>
2026-02-20 20:18:38 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,26 @@
+# def add_numbers(a,b):
+#     sum=a+b
+#     print('sum:',sum)
+# add_numbers(2,3)
+
+# def add_numbers(a=7,b=8):
+#     sum=a+b
+#     print("Sum:",sum)
+
+# add_numbers(2,3)
+# add_numbers(a=2)
+# add_numbers()
+
+# def display_info(first_name,last_name):
+#     print("First Name:",first_name)
+#     print("Last Name:", last_name)
+# display_info(last_name="Cartman",first_name="Eric")
+
+# def find_sum(*numbers):
+#     result=0
+#     for num in numbers:
+#         result=result+num
+#     print("Sum=",result)
+        
+# find_sum(1,2,3)
+# find_sum(4,9)
