5a22687eb5474e38066b012abc2baa09e1cfae33
Navya00301 <navyamaria2005@gmail.com>
2026-02-23 16:17:02 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,64 @@
+# thislist=["apple","banana","cherry"]
+# print(thislist)
+
+# print(len(thislist))
+
+# print(type(thislist))
+
+# thislist=["apple","banana","cherry","orange","kiwi","melon","mango"]
+# print(thislist[2:5])
+
+# thislist=["apple","banana","cherry"]
+# thislist[1]="blackcurrant"
+# print(thislist)
+
+# thislist=["apple","banana","cherry","orange","kiwi","mango"]
+# thislist[1:3]=["blackcurrant","watermelon"]
+# print(thislist)
+
+# thislist.append("orange")
+# print(thislist)
+
+# thislist=["apple","banana","cherry"]
+# tropical=["mango","pineapple","papaya"]
+# thislist.extend(tropical)
+# print(thislist)
+
+# thislist.remove("banana")
+# print(thislist)
+
+# thislist.pop(1)
+# print(thislist)
+
+# thislist.pop()
+# print(thislist)
+
+# del thislist[0]
+# print(thislist)
+
+# for x in thislist:
+#     print(x)
+
+# fruits=["apple","banana","cherry","kiwi","mango"]
+# newlist=[]
+# for x in fruits:
+#     if "a" in x:
+#         newlist.append(x)
+# print(newlist)
+
+# newlist=[x for x in fruits if "a" in x]
+# print(newlist)
+
+# list1=["a","b","c"]
+# list2=[1,2,3]
+# list3=list1+list2
+# print(list3)
+
+# for x in list2:
+#     list1.append(x)
+# print(list1)
+
+# fruits=["apple","banana","cherry"]
+# fruits.reverse()
+# print(fruits)
+
