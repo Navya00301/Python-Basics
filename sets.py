345c64567d4f8d11f60b9e63342c94951faabb00
Navya00301 <navyamaria2005@gmail.com>
2026-02-24 16:31:22 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,138 @@
+# thisset={"apple","banana","cherry"}
+# print(thisset)
+
+# thisset={"apple","banana","cherry",True,1,2}
+# print(thisset)
+
+# thisset={"apple","banana","cherry",False,0,1}
+# print(thisset)
+
+# thisset={"apple","banana","cherry"}
+# print(len(thisset))
+
+# print(type(thisset))
+
+# for x in thisset:
+    # print(x)
+
+# print("banana" in thisset)
+
+# print("banana" not in thisset)
+
+# thisset.add("plum")
+# print(thisset)
+
+# tropical={"pineapple","mango","papaya"}
+# thisset.update(tropical)
+# print(thisset)
+
+# mylist=["kiwi","orange"]
+# thisset.update(mylist)
+# print(thisset)
+
+# thisset.remove("banana")
+# print(thisset)
+
+# thisset.discard("banana")
+# print(thisset)
+
+# x=thisset.pop()
+# print(x)
+# print(thisset)
+
+# thisset.clear()
+# print(thisset)
+
+# del thisset
+# print(thisset)
+
+
+# set3=set1.union(set2)
+# print(set3)
+
+# set3=set1|set2
+# print(set3)
+
+# set3=set1.intersection(set2)
+# print(set3)
+
+# set3=set1&set2
+# print(set3)
+
+# set3=set1.difference(set2)
+# print(set3)
+
+# set3=set1.symmetric_difference(set2)
+# print(set3)
+
+# x=frozenset({"apple","banana","cherry"})
+# print(x)
+# print(type(x))
+
+# thisdict={
+
+#  "brand":"Ford",
+#  "model":"Mustang",
+#  "years" : 1964
+# }
+# print(thisdict)
+
+# print(thisdict["brand"])
+
+# x=thisdict.get("model")
+# print(x)
+
+# x=thisdict.keys()
+# print(x)
+
+# x=thisdict.values()
+# print(x)
+
+# x=thisdict.items()
+# print(x)
+
+# thisdict["years"]=2018
+# print(thisdict)
+
+# thisdict.update({"years":2020})
+# print(thisdict)
+
+# thisdict["colour"]="red"
+# print(thisdict)
+
+# thisdict.pop("model")
+# print(thisdict)
+
+# thisdict.popitem()
+# print(thisdict)
+
+# del thisdict["model"]
+# print(thisdict)
+
+# thisdict.clear()
+# print(thisdict)
+
+# for x in thisdict.values():
+#     print(x)
+
+# for x in thisdict.keys():
+#     print(x)
+
+# for x,y in thisdict.items():
+#     print(x,y)
+
+# myfamily={
+#     "child1":{
+#         "name":"Tobias",
+#         "years":2007
+#     },
+#     "child2":{
+#         "name":"Emil",
+#         "years":2004
+#     },
+#     "child3":{
+#         "name":"Linus",
+#         "years":2011
+#     }
+# }
+# print(myfamily)
---
+++ tuple.py
@@ -0,0 +1,63 @@
+# x=("apple","banana","cherry")
+# y=list(x)
+# y[1]="kiwi"
+# x=tuple(y)
+# print(x)
+
+# thistuple=("apple","banana","cherry")
+# y=list(thistuple)
+# y.append("orange")
+# thistuple=tuple(y)
+# print(thistuple)
+
+# y=("orange",)
+# thistuple +=y
+# print(thistuple)
+
+# y=list(thistuple)
+# y.remove("apple")
+# thistuple=tuple(y)
+# print(thistuple)
+
+# fruits= ("apple","banana","cherry")
+# mytuple=fruits*2
+# print(mytuple)
+
+
+# thistuple=(1,3,7,8,7,5,4,6,8,5)
+# x=thistuple.count(5)
+# print(x)
+
+# x=thistuple.index(8)
+# print(x)
+
+# x=("2,4,6,8,10")
+# print(x)
+
+# thistuple=(1,3,5,7,9)
+# print(thistuple[2])
+
+# print(thistuple[-1])
+
+# print(thistuple[1:5])
+
+# print(len(thistuple))
+
+# print(3 in thistuple)
+
+# tuple1=(1,2,3,4,5)
+# tuple2=(6,7,8,9,10)
+# tuple3= tuple1+tuple2
+# print(tuple3)
+
+# fruits=("cherry","banana","orange")
+# mytuple=fruits*3
+# print(mytuple)
+
+# x=(1,2,3,4,5)
+# y=list(x)
+# print(y)
+
+x=[1,2,3,4,5]
+y=tuple(x)
+print(y)
